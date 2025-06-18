7a54c29abae9fd3e0c31b21bdccf075e467676e1
demdil <dilaevmaks@gmail.com>
2025-06-18 17:41:33 +0400

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,7 +1,15 @@
 <?php
+header('Content-Type: application/json');
+
 $to = $_POST['to'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
+$headers = "From: no-reply@yourdomain.com\r\n";
+$headers .= "Content-Type: text/html; charset=utf-8\r\n"; // Важно для HTML-писем
 
-mail($to, $subject, $message);
+if (mail($to, $subject, $message, $headers)) {
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['error' => 'Ошибка отправки']);
+}
 ?>
