c968d2f55e15a1754f25d2aa5e2bace572560669
demdil <dilaevmaks@gmail.com>
2025-06-18 18:05:16 +0400

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,7 +1,18 @@
 <?php
+$log = date('Y-m-d H:i:s') . " | To: " . $_POST['to'] . "\n";
+file_put_contents('mail.log', $log, FILE_APPEND);
+
 $to = $_POST['to'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
+$headers = "From: no-reply@yourdomain.com\r\n";
+$headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-mail($to, $subject, $message);
+if(mail($to, $subject, $message, $headers)) {
+    file_put_contents('mail.log', "SUCCESS\n", FILE_APPEND);
+    echo json_encode(['success' => true]);
+} else {
+    file_put_contents('mail.log', "FAILED\n", FILE_APPEND);
+    echo json_encode(['error' => 'Send failed']);
+}
 ?>
